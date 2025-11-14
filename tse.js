8d18e38aac8a30010c926dc1232a4b601f68c37e
Caleb <caleb.anderson@ecolab.com>
2025-11-14 10:30:51 -0500

    Basic Updates for Fixing git push

--- tse.js
+++ tse.js
@@ -5,14 +5,17 @@ import {
   LiveboardEmbed
 } from "https://unpkg.com/@thoughtspot/visual-embed-sdk/dist/tsembed.es.js";
 
+// Save these imports for TrustedAuthTokenCookieless
+// import "./styles.css";
+// import { getTokenService } from "./tokenService.js";
+// import { debounceFunc } from "./utils.js";
+
 // PROD liveboard: 30c73d7d-b735-4c27-a4fa-5a3f9f24698f
 // DEV liveboard: dab294fa-9717-480e-9cf7-b4e80e17813c
-// DEV in DEV liveboard: 
+// DEV DEV liveboard: 7ccbaa31-d17c-4f8d-9b9b-133a6298f134
 
 const tsURL = "https://ecolab-dev.thoughtspot.cloud";
-const lbID = "";
-
-
+const lbID = "7ccbaa31-d17c-4f8d-9b9b-133a6298f134";
 
 // Initializes the application with ThoughtSpot
 const loadApp = () => {
@@ -21,9 +24,10 @@ const loadApp = () => {
     init({
       thoughtSpotHost: tsURL,
       authType: AuthType.None,
+      // getAuthToken: getTokenService,
+      // disableTokenVerification: true,
     });
-    console.log(`(${lbID}) is the liveboard`);
-    console.log("SDK initialized successfully with AuthType.None.");
+    console.log("SDK initialized successfully with EmbeddedSSO.");
   } catch (error) {
     console.error("Error during SDK initialization:", error);
     return;
@@ -44,8 +48,21 @@ const embedLiveboard = () => {
     console.log("Embed container found. Proceeding with Liveboard embedding...");
 
     const liveboardEmbed = new LiveboardEmbed(embedContainer, {
-      frameParams: { width: '100%', height: '800px', style: 'border:none;' },
+      frameParams: { width: '100%', height: '100%', style: 'border:none;' },
       liveboardId: lbID,
+      fullHeight: true,
+      lazyLoadingForFullHeight: true,
+      /* Show or hide the liveboard title and description */
+      showLiveboardTitle: true,
+      showLiveboardDescription: false,
+      isLiveboardHeaderSticky: false, /* Removes the header stickiness when scrolling if set to false */
+      // hideLiveboardHeader: false, /* Hides the entire liveboard header */
+
+      /* Customize display of tabs in the liveboard header */
+      // hideTabPanel: false, /* Removes the tab panel */
+      // visibleTabs:['TabId'], /* Remove all tabs if empty array*/
+      // hiddenTabs:['TabId'],
+      /* Use either visibleTabs or hiddenTabs */
     });
 
     liveboardEmbed.render()
